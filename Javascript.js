c0856e916523517455d42faabcc6360b91c7b459
Yohan <yohanimmanuelhendrajaya@gmail.com>
2022-04-27 07:53:40 +0700

    k

--- Javascript.js
+++ Javascript.js
@@ -1,18 +1,32 @@
-function dropdown() {
-    var openDropdown = document.getElementsByClassName("sub-menu-1")[0];
-    console.log(openDropdown.style.display)
-    if(openDropdown.style.display === "") {
-        openDropdown.style.display = "block";
-    } else if (openDropdown.style.display === "none") {
-        openDropdown.style.display = "block";
+function chardown() {
+    var charDropdown = document.getElementsByClassName("sub-menu-1")[0];
+    console.log(charDropdown.style.display)
+    if(charDropdown.style.display === "") {
+        charDropdown.style.display = "block";
+    } else if (charDropdown.style.display === "none") {
+        charDropdown.style.display = "block";
     } else {
-        openDropdown.style.display = "none";
-    }
-    // console.log(openDropdown.classList.contains('show'))
+        charDropdown.style.display = "none";
+}}
 
-    // if (openDropdown.classList.contains('show')) {
-    //     openDropdown.classList.remove('show');
-    // } else {
-    //     openDropdown.classList.add('show')
-    // }
-}
+function weapondown() {
+    var weaponDropdown = document.getElementsByClassName("sub-menu-2")[0];
+    console.log(weaponDropdown.style.display)
+    if(weaponDropdown.style.display === "") {
+        weaponDropdown.style.display = "block";
+    } else if (weaponDropdown.style.display === "none") {
+        weaponDropdown.style.display = "block";
+    } else {
+        weaponDropdown.style.display = "none";
+}}
+
+function artifdown() {
+    var artifDropdown = document.getElementsByClassName("sub-menu-3")[0];
+    console.log(artifDropdown.style.display)
+    if(artifDropdown.style.display === "") {
+        artifDropdown.style.display = "block";
+    } else if (artifDropdown.style.display === "none") {
+        artifDropdown.style.display = "block";
+    } else {
+        artifDropdown.style.display = "none";
+}}
